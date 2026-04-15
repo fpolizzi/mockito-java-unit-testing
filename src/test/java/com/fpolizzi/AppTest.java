4539c41dad0ef7f5c0ace2fa2c8378fcf1fa6dff
Franco Polizzi <me@fpolizzi.com>
2026-04-15 02:26:31 +0200

    test: first example of a mock

--- src/test/java/com/fpolizzi/AppTest.java
+++ src/test/java/com/fpolizzi/AppTest.java
@@ -2,14 +2,38 @@ package com.fpolizzi;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 /**
  * Created by fpolizzi on 15.04.26
  */
 class AppTest {
 
-    @Test
-    void myFirstTest() {
+    private final List<String> myMock = mock();
+    private final List<String> names = new ArrayList<>();
 
+    @Test
+    void myFirstTestWithoutMock() {
+
+        names.add("hello");
+        assertThat(names).hasSize(1);
     }
 
+    @Test
+    void myFirstTestWithMock() {
+
+        myMock.add("hello");
+        when(myMock.get(0)).thenReturn("hello");
+
+        verify(myMock).add("hello");
+
+        String actual = myMock.get(0);
+        assertThat(actual).isEqualTo("hello");
+    }
 }

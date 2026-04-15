ac83b5344d4503c05ff5bb3acb4532796e3ddc0f
Franco Polizzi <me@fpolizzi.com>
2026-04-16 00:20:13 +0200

    test: add verify() to canCharge

--- src/test/java/com/fpolizzi/OrderServiceTest.java
+++ src/test/java/com/fpolizzi/OrderServiceTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by fpolizzi on 15.04.26
@@ -29,12 +29,13 @@ class OrderServiceTest {
 
         // given
         BigDecimal amount = BigDecimal.TEN;
-        when(paymentProcessor.charge(amount)).thenReturn(true);
+        when(paymentProcessor.charge(any())).thenReturn(true);
 
         // when
         boolean actual = underTest.processOrder(amount);
 
         // then
+        verify(paymentProcessor).charge(amount);
         assertThat(actual).isTrue();
     }
 }

c140a89d81a74d90dbb84b8ca67d3821233a7cc9
Franco Polizzi <me@fpolizzi.com>
2026-04-18 20:55:00 +0200

    refactor: rename canCharge to cannChargeWithAssertArg

--- src/test/java/com/fpolizzi/OrderServiceTest.java
+++ src/test/java/com/fpolizzi/OrderServiceTest.java
@@ -3,6 +3,7 @@ package com.fpolizzi;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -38,7 +39,7 @@ class OrderServiceTest {
     }
 
     @Test
-    void canCharge() {
+    void canChargeWithAssertArg() {
 
         // given
         BigDecimal amount = BigDecimal.TEN;
